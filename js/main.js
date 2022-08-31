d8c57d6394c3b3760923f1a668170a23913ee0e3
Luca Boffo <83424178+lucaboffo@users.noreply.github.com>
2022-08-31 12:43:17 +0200

    Create a page and a single page like a template

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Trigger header_menu
 $(document).ready(function(){
-    $(".header_icon-bar").click(function(e){
-        $(".header_menu").toggleClass('is-open');
+    $(".header__icon-bar").click(function(e){
+        $(".header__menu").toggleClass('is-open');
         e.preventDefault();
     })
 });
